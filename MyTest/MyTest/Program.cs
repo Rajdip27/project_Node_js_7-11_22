8abbd2e3495df9098c3527f153f11be699d31d72
Raj <srajdip920@gmail.com>
2022-11-08 01:03:56 +0600

    fist

---
+++ MyTest/MyTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person per = new person();
+            per.firstName = "Rajdip";
+            per.MiddleNamw = "Raj";
+            per.LastName = "Santanu";
+            string fullName = per.GetFullName();
+            Console.WriteLine(fullName);
+
+
+            person per2 = new person();
+            per2.firstName = "Eava";
+            per2.MiddleNamw = "Kabila";
+            per2.LastName = "Rokeya";
+            fullName = per2.GetFullName();
+            Console.WriteLine(fullName);
+            person per3;
+            per3 = new person();
+            per3 = per;
+            per3.MiddleNamw = "Santanu";
+            fullName = per3.GetFullName();
+            Console.WriteLine(fullName);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MyTest/MyTest/person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+    class person
+    {
+        public string firstName;
+        public string MiddleNamw;
+        public string LastName;
+
+
+        public string GetFullName()
+        {
+            string fullName = firstName + " " + MiddleNamw + " " + LastName;
+            return fullName;
+
+        }
+
+
+
+    }
+}
